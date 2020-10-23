49a85ea036e8514b4cb32856e359d6a669bfd91d
Juan Manuel PM <juanmanuel.perezmenendez@ing.austral.edu.ar>
2020-10-23 01:02:23 -0300

    boludeces

--- src/TpProg2/Users/AMBGeneral.java
+++ src/TpProg2/Users/AMBGeneral.java
@@ -3,13 +3,30 @@ package TpProg2.Users;
 import TpProg2.DataStore.DataStore;
 import TpProg2.Events.Disease;
 import TpProg2.Events.Symptom;
+import TpProg2.Exceptions.ABMAdminException;
+import TpProg2.Exceptions.ABMCitizenException;
+import TpProg2.Exceptions.ABMUserException;
+import TpProg2.ImplementOfUsers.Zone;
+import TpProg2.util.Scanner;
+import TpProg2.util.UserInterface;
+import java.util.ArrayList;
 
 public class AMBGeneral {
-    DataStore<Disease> dataStoreDiseases;
-    DataStore<Symptom> dataStoreSymptoms;
+    DataStore<Citizen> anses;
+    ABMAdmin adminABM;
+    ABMCitizen citizenABM;
+    ArrayList<Symptom> symptoms;
+    Disease disease; // No usamos una lista de enfermedades por ahora, solo hay una.
+    ArrayList<Zone> zones;
 
-    public AMBGeneral(DataStore<Disease> dataStoreDiseases, DataStore<Symptom> dataStoreSymptoms) {
-        this.dataStoreDiseases = dataStoreDiseases;
-        this.dataStoreSymptoms = dataStoreSymptoms;
+    public AMBGeneral(DataStore<Citizen> anses, ABMAdmin adminABM, ABMCitizen citizenABM, ArrayList<Symptom> symptoms, Disease disease, ArrayList<Zone> zones) {
+        this.anses = anses;
+        this.adminABM = adminABM;
+        this.citizenABM = citizenABM;
+        this.symptoms = symptoms;
+        this.disease = disease;
+        this.zones = zones;
     }
+
+    //aca irian los metodos de iniciar sesion y registrarse entre otros
 }
